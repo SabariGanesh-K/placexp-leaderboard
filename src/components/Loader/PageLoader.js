e5cf4b27beebee54bd9aef59e79c527b503dc672
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2022-09-17 10:12:43 +0530

    loader odified

--- src/components/Loader/PageLoader.js
+++ src/components/Loader/PageLoader.js
@@ -4,18 +4,18 @@ const PageLoader = () =>{
     return(
       <div className='flex flex-row justify-center '>
        <div className='flex flex-row justify-center h-screen '>
-        <div className='flex flex-col justify-center text-red-700'>M
-          <div className = " animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-red-700">  </div>
-         M</div></div>
+        <div className='flex flex-col justify-center text-purple-700'>P
+          <div className = " animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-purple-700">  </div>
+         P</div></div>
           <div className='flex flex-row justify-center h-screen'>
-          <div className='flex flex-col justify-center text-red-700'>I
-          <div className = "animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-red-700"></div>
+          <div className='flex flex-col justify-center text-purple-700'>X
+          <div className = "animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-purple-700"></div>
           <div className = "animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-blue-700"></div>
-          I</div></div>
+          X</div></div>
           <div className='flex flex-row justify-center h-screen'>
-          <div className='flex flex-col justify-center text-red-700'>C
-          <div className = "animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-red-700"></div>
-          C</div></div>
+          <div className='flex flex-col justify-center text-purple-700'>P
+          <div className = "animate-spin rounded-full h-16 w-16 border-b-2 top-0 left-0 border-purple-700"></div>
+          P</div></div>
           </div>
         
   
